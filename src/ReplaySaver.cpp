43289f11022c2d3262c2dcc48b3b27550ae8ead5
Crxptos <planeseven7@gmail.com>
2026-03-24 16:00:43 +0000

    Update ReplaySaver.cpp

--- src/ReplaySaver.cpp
+++ src/ReplaySaver.cpp
@@ -1,5 +1,6 @@
 #include "ReplaySaver.hpp"
 #include <Geode/Geode.hpp>
+
 #include <fstream>
 #include <filesystem>
 #include <thread>
@@ -8,33 +9,43 @@ using namespace geode::prelude;
 
 bool ReplaySaver::saveReplay(const ReplayData& data, std::string& outPath) {
     try {
-        std::string base = Mod::get()->getSaveDir().string();
-        std::string folder = base + "/savedReplays/";
+        auto base = Mod::get()->getSaveDir();
+        std::filesystem::path folder = base / "savedReplays";
+
         std::filesystem::create_directories(folder);
 
-        outPath = folder + "level_" + std::to_string(data.levelID) + ".replay";
+        std::filesystem::path filePath =
+            folder / ("level_" + std::to_string(data.levelID) + ".replay");
 
-        std::ofstream out(outPath);
+        std::ofstream out(filePath);
+
+        if (!out.is_open()) {
+            log::error("Failed to open replay file!");
+            return false;
+        }
 
         out << data.levelID << "\n";
-        for (auto& i : data.inputs) {
-            out << i.frame << " " << i.button << " " << i.pressed << "\n";
+
+        for (const auto& i : data.inputs) {
+            out << i.frame << " "
+                << i.button << " "
+                << i.pressed << "\n";
         }
 
         out.close();
+        outPath = filePath.string();
+
         return true;
 
-    } catch (...) {
+    } catch (const std::exception& e) {
+        log::error("Replay save error: {}", e.what());
         return false;
     }
 }
 
 void ReplaySaver::exportMP4(const std::string& inputFolder, const std::string& output) {
-    std::thread([=]() {
-        // 🔧 Replace this with FFmpeg API later
-        // Example:
-        // FFmpeg::encodeVideo(inputFolder + "/frame_%d.png", output, 60);
-
-        log::info("Exporting MP4: {}", output);
+    std::thread([inputFolder, output]() {
+        // 🔧 Replace later with FFmpeg API
+        log::info("Exporting MP4 from {} -> {}", inputFolder, output);
     }).detach();
 }
