e2309ad83bf303f942299d42d3d8d73006c688f5
Crxptos <planeseven7@gmail.com>
2026-03-24 07:46:39 +0000

    Create ReplayRecorder.hpp

---
+++ src/ReplayRecorder.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "ReplayData.hpp"
+
+class ReplayRecorder {
+public:
+    static ReplayRecorder* get();
+
+    void start(int levelID);
+    void stop();
+    void update();
+
+    void recordPress(int button);
+    void recordRelease(int button);
+
+    ReplayData& getData();
+
+private:
+    ReplayData m_data;
+    int m_frame = 0;
+    bool m_recording = false;
+};
